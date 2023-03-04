c50e71b91965e4ce1af85bbbae8384850164f734
Samuel <samuel@samuelvafanamac.myfiosgateway.com>
2023-03-04 16:27:32 -0500

    bannercss

--- src/Banner.js
+++ src/Banner.js
@@ -6,17 +6,38 @@ import "./Banner.css";
 
 
 function Banner() {
+
+
+  function truncate(string, n) {
+
+    return string?.length > n ? string.substr(0, n - 1) + "..." : string;
+
+  }
+
+
   return (
     <header
       className="banner"
       style={{
         backgroundSize: "cover",
-        backgroundImage: `url("https://cdn.freebiesupply.com/logos/large/2x/netflix-logo-png-transparent.png")`,
+        backgroundImage: `url("https://cdn.worldvectorlogo.com/logos/netflix.svg")`,
         backgroundPosition: "center center",
       }}
 
     >
+      <div className='banner__contents'>
+        <hi className="banner__title">Movie Name</hi>
+        <div className='banner__buttons'>
+          <button className='banner__button'>Play</button>
+          <button className='banner__button'>My List</button>
+        </div>
 
+
+        <div className='banner__description'>This is a test discription
+        </div>
+        <div className='banner__fadeBottom' />
+
+      </div>
 
     </header>
 
